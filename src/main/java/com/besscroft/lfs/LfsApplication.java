e26e241b63e5559fe96a74a316344f11c16d0d8e
besscroft <besscroft@foxmail.com>
2021-11-04 16:50:07 +0800

    fix:调整启动打印日志

--- src/main/java/com/besscroft/lfs/LfsApplication.java
+++ src/main/java/com/besscroft/lfs/LfsApplication.java
@@ -1,14 +1,16 @@
 package com.besscroft.lfs;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class LfsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LfsApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  LFS服务启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+        log.info("(♥◠‿◠)ﾉﾞ  LFS服务启动成功   ლ(´ڡ`ლ)ﾞ  \n");
     }
 
 }
